7ee85a2c6392357c3e3d29363fc0a2336cb08145
Alan Curtis <talancurtis@gmail.com>
2018-03-27 18:04:24 -0600

    added react-native-payments and working need to add google account.

--- App.js
+++ App.js
@@ -2,15 +2,60 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button} from 'react-native';
 
+// react native payments needs: global.PaymentRequest = require('react-native-payments').PaymentRequest;
+global.PaymentRequest = require('react-native-payments').PaymentRequest;
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  test(){
+    console.warn('Test pushed')
+  }
+  handlePay(){
+    console.warn('handle press')
+
+    const METHOD_DATA = [{
+      supportedMethods: ['android-pay'],
+      data: {
+        supportedNetworks: ['visa', 'mastercard', 'amex'],
+        currencyCode: 'USD',
+        environment: 'TEST', // defaults to production
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'NETWORK_TOKEN',
+          parameters: {
+            publicKey: 'your-pubic-key'
+          }
+        }
+      }
+    }];
+
+    const DETAILS = {
+      id: 'basic-example',
+      displayItems: [
+        {
+          label: 'Movie Ticket',
+          amount: { currency: 'USD', value: '15.00' }
+        }
+      ],
+      total: {
+        label: 'Merchant Name',
+        amount: { currency: 'USD', value: '15.00' }
+      }
+    };
+
+    const paymentRequest = new PaymentRequest(METHOD_DATA, DETAILS);
+
+    paymentRequest.show().then(paymentResponse => paymentResponse.complete('success'));
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Button title='Pay Me'></Button>
+        <Button title='Pay Me' onPress={()=>this.handlePay()}></Button>
       </View>
     );
   }
